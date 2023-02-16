b3b62b3b4fb1998434c95e3e5f702dbfc4fa8c20
Sergey <sergilio-p@yandex.ru>
2023-02-16 16:20:21 +0300

    Классы

--- script.js
+++ script.js
@@ -958,3 +958,39 @@
 
 // console.log(double(3));
 // console.log(double(13));
+
+
+// ******************** Классы ***********************
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+// const squere = new Rectangle(10, 10);
+// const long = new Rectangle(20, 100);
+// console.log(squere.calcArea());
+// console.log(long.calcArea());
+
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'hello world', 'red');
+
+div.showMyProps();
+console.log(div.calcArea())
